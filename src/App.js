d55253e65ca26df1db1fb286e10aba6133b443e6
Robert Amour <robert@ferretech.io>
2020-03-29 00:11:21 -0700

    Use webm versions of animations

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ const useStyles = makeStyles((theme) =>
       display: 'table-cell',
       verticalAlign: 'middle',
       marginTop: theme.spacing(2),
-      marginBottom: theme.spacing(2),
+      marginBottom: theme.spacing(2)
     },
     descriptionContainer: {},
     technology: {
@@ -191,66 +191,66 @@ function App () {
     return () => clearInterval(avatarFloatInterval)
   })
   return (
-    <Grid container className={classes.root} alignItems='space-around' direction='column'>
-      <Paper className={classes.topHeader} elevation={6}>
-        <Grid container className={classes.headerGrid} alignItems='center'>
+    <Grid container className={ classes.root } alignItems='space-around' direction='column'>
+      <Paper className={ classes.topHeader } elevation={ 6 }>
+        <Grid container className={ classes.headerGrid } alignItems='center'>
           <Grid item container justify='center'>
-            <Link className={classes.link}
+            <Link className={ classes.link }
                   href='Robert-Amour-resume.pdf'>Resume</Link>
-            <Link className={classes.link} href='#bio'>Bio</Link>
-            <Link className={classes.link} href='#projects'>Projects</Link>
+            <Link className={ classes.link } href='#bio'>Bio</Link>
+            <Link className={ classes.link } href='#projects'>Projects</Link>
           </Grid>
         </Grid>
       </Paper>
       <Grid item container alignContent='center'>
-        <Paper elevation={avatarElevation} className={classes.profileCard}>
+        <Paper elevation={ avatarElevation } className={ classes.profileCard }>
           <Grid item>
-            <Avatar alt='Robert Amour' className={classes.profile} variant='circle' src='profile.jpg' />
+            <Avatar alt='Robert Amour' className={ classes.profile } variant='circle' src='profile.jpg' />
           </Grid>
-          <Grid item className={classes.textContainer}>
-            <Typography className={classes.title} variant='h3' align='center'>
+          <Grid item className={ classes.textContainer }>
+            <Typography className={ classes.title } variant='h3' align='center'>
               Robert Amour
             </Typography>
-            <Typography className={classes.subTitle} variant='h5' align='center'>
+            <Typography className={ classes.subTitle } variant='h5' align='center'>
               Full Stack Developer
             </Typography>
           </Grid>
         </Paper>
       </Grid>
       <Grid item id='bio'>
-        <Typography className={classes.bioHeader} align='center' variant='h2'>
+        <Typography className={ classes.bioHeader } align='center' variant='h2'>
           Hi! I'm Robert (or Bobby).
         </Typography>
-        <Typography className={classes.bioSubHeader} align='center' variant='h5'>
+        <Typography className={ classes.bioSubHeader } align='center' variant='h5'>
           I like playing piano, cooking, baking, and making awesome software.
         </Typography>
         <Grid item alignContent='center'>
-          <Typography className={classes.bio} align='center'>
+          <Typography className={ classes.bio } align='center'>
             I spent a few years pursuing a degree in music before deciding my interest in computer science
             and programming was more than just a hobby. A few toy projects and one Barnes-Hut N-body simulator later, I
             decided to pursue programming with a renewed vigor.
           </Typography>
-          <Typography className={classes.bioLast} align='center'>
+          <Typography className={ classes.bioLast } align='center'>
             I love simple, functional design with great UX.
           </Typography>
-          <Typography className={classes.ornament} align='center' variant='h2'>
+          <Typography className={ classes.ornament } align='center' variant='h2'>
             ❦
           </Typography>
         </Grid>
       </Grid>
-      <Grid id='projects' item container direction='column' className={classes.projects}>
-        <Typography className={classes.header} variant='h3' align='center'>
+      <Grid id='projects' item container direction='column' className={ classes.projects }>
+        <Typography className={ classes.header } variant='h3' align='center'>
           Projects:
         </Typography>
-        <Paper elevation={4} className={classes.sectionPaper}>
+        <Paper elevation={ 4 } className={ classes.sectionPaper }>
           <Grid item container direction='row'>
-            <Grid item container sm={12} md={6} alignItems='stretch' className={classes.descriptionContainer}>
+            <Grid item container sm={ 12 } md={ 6 } alignItems='stretch' className={ classes.descriptionContainer }>
               <Grid item container>
                 <Grid item>
-                  <Typography className={classes.subHeader} variant='h4'>
+                  <Typography className={ classes.subHeader } variant='h4'>
                     Typometry
                   </Typography>
-                  <Typography className={classes.timeline}>
+                  <Typography className={ classes.timeline }>
                     October 2018 - present
                   </Typography>
                 </Grid>
@@ -268,57 +268,59 @@ function App () {
                 </Grid>
               </Grid>
               <Grid item>
-                <Typography variant='body1' className={classes.projectBody}>
+                <Typography variant='body1' className={ classes.projectBody }>
                   Typometry is a web app for improving typing speed at all levels by assessing typing characteristics
                   and seamlessly delivering personalized exercises.
                 </Typography>
-                <Typography variant='body1' className={classes.projectBody}>
+                <Typography variant='body1' className={ classes.projectBody }>
                   Typometry is my attempt at hacking touch typing. As the backend receives granular typing data,
                   simple, corrective exercises are generated in realtime to fix the "weakest link" in the user's typing
                   technique. This way, a typist with a non-traditional typing style does not have to start from scratch
                   learning which fingers go where.
                 </Typography>
               </Grid>
-              <Grid item className={classes.technology}>
-                <Typography className={classes.technologyHeader} variant='h5'>
+              <Grid item className={ classes.technology }>
+                <Typography className={ classes.technologyHeader } variant='h5'>
                   Frontend:
                 </Typography>
-                <Typography className={classes.technologyItem} variant='subtitle1'>
+                <Typography className={ classes.technologyItem } variant='subtitle1'>
                   React
                 </Typography>
-                <Typography className={classes.technologyHeader} variant='h5'>
+                <Typography className={ classes.technologyHeader } variant='h5'>
                   Backend:
                 </Typography>
-                <Typography className={classes.technologyItem} variant='subtitle1'>
+                <Typography className={ classes.technologyItem } variant='subtitle1'>
                   Django + Postgres
                 </Typography>
               </Grid>
             </Grid>
-            <Grid item sm={12} md={6}>
-              <img src='typometry.gif' alt='Animated GIF of typing practice with Typometry' className={classes.gif} />
+            <Grid item sm={ 12 } md={ 6 }>
+              <video controls autoPlay>
+                <source src='typometry.webm' type='video/webm' className={ classes.gif } />
+              </video>
             </Grid>
           </Grid>
         </Paper>
-        <Paper elevation={4} className={classes.sectionPaper}>
+        <Paper elevation={ 4 } className={ classes.sectionPaper }>
           <Grid item container direction='row'>
-            <Grid item container sm={12} md={6} alignItems='stretch' className={classes.descriptionContainer}>
+            <Grid item container sm={ 12 } md={ 6 } alignItems='stretch' className={ classes.descriptionContainer }>
               <Grid item container>
                 <Grid item>
-                  <Typography className={classes.subHeader} variant='h4'>
+                  <Typography className={ classes.subHeader } variant='h4'>
                     eFerret
                   </Typography>
-                  <Typography className={classes.timeline}>
+                  <Typography className={ classes.timeline }>
                     December 2017 - present
                   </Typography>
                 </Grid>
                 <Grid item>
                   <Tooltip title='eFerret.io'>
-                    <IconButton className={classes.button} target='_blank' rel='noreferrer' href='https://eferret.io'>
+                    <IconButton className={ classes.button } target='_blank' rel='noreferrer' href='https://eferret.io'>
                       <LinkIcon />
                     </IconButton>
                   </Tooltip>
                   <Tooltip title='Check it out on Github'>
-                    <IconButton className={classes.button} target='_blank' rel='noreferrer'
+                    <IconButton className={ classes.button } target='_blank' rel='noreferrer'
                                 href='https://github.com/KunstDerFuge/eFerret-web-frontend'>
                       <GitHubIcon />
                     </IconButton>
@@ -327,114 +329,117 @@ function App () {
               </Grid>
               <a target='_blank' href='https://play.google.com/store/apps/details?id=io.eferret.eferret'
                  rel="noopener noreferrer">
-                <img className={classes.imageButton} alt='Get it on Google Play' src='google-play.png' />
+                <img className={ classes.imageButton } alt='Get it on Google Play' src='google-play.png' />
               </a>
-              <Typography variant='body1' className={classes.projectBody}>
+              <Typography variant='body1' className={ classes.projectBody }>
                 eFerret is a native Android app and modern web app (eFerret.io) for finding great deals and rare items
                 on eBay. Utilizing live data from the eBay API, it notifies users of interesting items within seconds
                 after they are posted.
               </Typography>
-              <ul className={classes.technologyItem}>
+              <ul className={ classes.technologyItem }>
                 <li>1000+ installs on Google Play</li>
                 <li>~ 75 daily active users, 250 monthly</li>
                 <li>Drives over $450,000 in sales to eBay every year</li>
                 <li>Processes ~1 million eBay API requests per day</li>
               </ul>
-              <Grid item className={classes.technology}>
-                <Typography className={classes.technologyHeader} variant='h5'>
+              <Grid item className={ classes.technology }>
+                <Typography className={ classes.technologyHeader } variant='h5'>
                   Frontend:
                 </Typography>
-                <Typography className={classes.technologyItem} variant='subtitle1'>
+                <Typography className={ classes.technologyItem } variant='subtitle1'>
                   React + Typescript (web)
                 </Typography>
-                <Typography className={classes.technologyItem} variant='subtitle1'>
+                <Typography className={ classes.technologyItem } variant='subtitle1'>
                   Java (Android)
                 </Typography>
-                <Typography className={classes.technologyHeader} variant='h5'>
+                <Typography className={ classes.technologyHeader } variant='h5'>
                   Backend:
                 </Typography>
-                <Typography className={classes.technologyItem} variant='subtitle1'>
+                <Typography className={ classes.technologyItem } variant='subtitle1'>
                   Django + Postgres + Django Rest Framework
                 </Typography>
-                <Typography className={classes.technologyItem}>
+                <Typography className={ classes.technologyItem }>
                   Celery / Redis task scheduling for push notification processing
                 </Typography>
               </Grid>
             </Grid>
-            <Grid item sm={12} md={6}>
-              <img src='eferret.gif' alt='Animated GIF of the eFerret Android app' className={classes.gif} />
+            <Grid item sm={ 12 } md={ 6 }>
+              <video controls autoPlay>
+                <source src='eferret.webm' type='video/webm' className={ classes.gif } />
+              </video>
             </Grid>
           </Grid>
         </Paper>
-        <Paper elevation={4} className={classes.sectionPaper}>
+        <Paper elevation={ 4 } className={ classes.sectionPaper }>
           <Grid item container direction='row'>
-            <Grid item container sm={12} md={6} alignItems='stretch' className={classes.descriptionContainer}>
+            <Grid item container sm={ 12 } md={ 6 } alignItems='stretch' className={ classes.descriptionContainer }>
               <Grid item container>
                 <Grid item>
-                  <Typography className={classes.subHeader} variant='h4'>
+                  <Typography className={ classes.subHeader } variant='h4'>
                     Heatwave (Freelance)
                   </Typography>
-                  <Typography className={classes.timeline}>
+                  <Typography className={ classes.timeline }>
                     April 2018 - present
                   </Typography>
                 </Grid>
                 <Grid item>
                   <Tooltip title='More info'>
-                    <IconButton className={classes.button} target='_blank' rel='noreferrer'
+                    <IconButton className={ classes.button } target='_blank' rel='noreferrer'
                                 href='https://www.chrisrentsch.com/home/GW/setup'>
                       <LinkIcon />
                     </IconButton>
                   </Tooltip>
                   <Tooltip title='Check it out on Github'>
-                    <IconButton className={classes.button} target='_blank' rel='noreferrer'
+                    <IconButton className={ classes.button } target='_blank' rel='noreferrer'
                                 href='https://github.com/rentcp/Heatwave'>
                       <GitHubIcon />
                     </IconButton>
                   </Tooltip>
                 </Grid>
               </Grid>
-              <Typography variant='body1' className={classes.projectBody}>
+              <Typography variant='body1' className={ classes.projectBody }>
                 "Project Heatwave" - A data-driven analysis of global warming using atmospheric radiation emission
                 data from NASA's AIRS program. This project gathers and analyzes a large dataset from the NASA AIRS
                 satellite using Pandas and Numpy. Heatwave is a fully open-source project by <a
                 href='https://www.chrisrentsch.com' target='_blank' rel="noopener noreferrer">Chris Rentsch</a>.
               </Typography>
-              <ul className={classes.technologyItem}>
+              <ul className={ classes.technologyItem }>
                 <li>Implemented major feature additions and bug fixes over the course of 14 months</li>
                 <li>Resulted in several rehires by the client for additional features, fixes, and consultation.</li>
               </ul>
-              <Grid item className={classes.technology}>
-                <Typography className={classes.technologyHeader} variant='h5'>
+              <Grid item className={ classes.technology }>
+                <Typography className={ classes.technologyHeader } variant='h5'>
                   Technologies used:
                 </Typography>
-                <Typography className={classes.technologyItem} variant='subtitle1'>
+                <Typography className={ classes.technologyItem } variant='subtitle1'>
                   Python
                 </Typography>
-                <Typography className={classes.technologyItem} variant='subtitle1'>
+                <Typography className={ classes.technologyItem } variant='subtitle1'>
                   Pandas
                 </Typography>
-                <Typography className={classes.technologyItem} variant='subtitle1'>
+                <Typography className={ classes.technologyItem } variant='subtitle1'>
                   Numpy
                 </Typography>
               </Grid>
             </Grid>
-            <Grid item sm={12} md={6}>
-              <img src='heatwave.gif' alt='Animated GIF of Heatwave being used from the command line'
-                   className={classes.gif} />
+            <Grid item sm={ 12 } md={ 6 }>
+              <video controls autoPlay>
+                <source src='heatwave.webm' type='video/webm' className={ classes.gif } />
+              </video>
             </Grid>
           </Grid>
         </Paper>
       </Grid>
-      <Paper className={classes.footer} elevation={6}>
-        <Grid container className={classes.headerGrid} alignItems='center'>
+      <Paper className={ classes.footer } elevation={ 6 }>
+        <Grid container className={ classes.headerGrid } alignItems='center'>
           <Grid item container justify='center'>
-            <Link className={classes.link}
+            <Link className={ classes.link }
                   href='https://github.com/KunstDerFuge'>Github</Link>
-            <Link className={classes.link} href='https://www.linkedin.com/in/robert-amour-5716b3105/'>LinkedIn</Link>
-            <Link className={classes.link} href='Robert-Amour-resume.pdf'>Resume</Link>
+            <Link className={ classes.link } href='https://www.linkedin.com/in/robert-amour-5716b3105/'>LinkedIn</Link>
+            <Link className={ classes.link } href='Robert-Amour-resume.pdf'>Resume</Link>
           </Grid>
           <Grid item container justify='center'>
-            <Link className={classes.link} href='#'>Back to top</Link>
+            <Link className={ classes.link } href='#'>Back to top</Link>
           </Grid>
         </Grid>
       </Paper>
